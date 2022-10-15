636e508aff5457a403a086b7e9e8984eb89ae139
Manish Singh <107620367+Manish0509@users.noreply.github.com>
2022-10-15 23:59:21 +0530

    Add files via upload

---
+++ Controllers/auth.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs')
+const router = express.Router()
+var utility = require('../utility/utility')
+
+var users = []
+
+router.post('/login', (req, res) => {
+    //Authenticate user
+    const email = req.body.email;
+    const password = req.body.password;
+    var filtered
+    fs.readFile(__dirname + "/../" + "user.json", 'utf8', function (err, data) {
+        users = JSON.parse(data)
+        filtered = users.filter((x) => {
+            return (x.email == email && utility.decrypt(x.password) == password)
+
+        });
+        if (filtered.length == 0) {
+            return res.json({ msg: 'Unauthorised user' })
+        }
+        var Token = utility.JWTgenerateToken(filtered[0])
+        return res.json({ Token: Token })
+    })
+})
+
+module.exports = router
---
+++ Controllers/blog.js
@@ -0,0 +1,71 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+const { v4: uuidv4 } = require('uuid');     // this is used to create random id
+const router = express.Router()
+var utility = require('../utility/utility')
+
+var postlist = []
+
+router.get('/', function (req, res) {
+    fs.readFile(__dirname + "/../" + "post.json", 'utf8', function (err, data) {
+        postlist = JSON.parse(data)
+        var sameuser = postlist.filter(function (p){
+            return p.createdBy==req.userId;
+        })
+            res.json(sameuser);
+        })
+});
+
+
+router.post('/', function (req, res) {
+    var newpost = req.body;
+    newpost.createdAt = new Date();
+    newpost.createdBy = req.userId   //encrypt the password entered by the user
+    newpost.id = uuidv4();   // this will create unique id
+    fs.readFile(__dirname + "/../" + "post.json", 'utf8', function (err, data) {
+        postlist = JSON.parse(data);   // parse is converting Javascript object to JSON string
+        postlist.push(newpost);
+        fs.writeFile(__dirname + "/../" + "post.json", JSON.stringify(postlist), function (err, writtenbytes) {
+            if (err) {
+                console.log('Cant write to file');
+            }
+            else {
+                console.log(' file has been written sucessfully');
+            }
+        })
+        res.end(JSON.stringify(newpost));   //stringify is converting sting to object
+    })
+})
+
+router.delete('/:id', function (req, res) {
+    fs.readFile(__dirname + "/../" + "post.json", 'utf8', function (err, data) {
+        postdelete = JSON.parse(data);
+        filtered = postdelete.findIndex((y) => y.id == req.params.id && y.createdBy==req.userId);
+        console.log(filtered)
+        if (filtered == '-1') {
+            return res.json({ msg: 'Invalid ID' })
+        } else {
+            postdelete.splice(filtered, 1)
+            fs.writeFile(__dirname + "/../" + "post.json", JSON.stringify(postdelete), function (err, writtenbytes) {
+                if (err) {
+                    console.log('Cant delete to file');
+                }
+            })
+            return res.json({ msg: 'deleted' })
+        }
+    });
+});
+
+router.get('/:id', function (req, res) {
+    fs.readFile(__dirname + "/../" + "post.json", 'utf8', function (err, data) {
+        var users = JSON.parse(data);   // parse is converting Javascript object to JSON string
+        var filtered = users.filter((x) => {
+            return (x.id == req.params.id)
+        });
+        console.log(req.params.id, filtered)
+        res.end(JSON.stringify(filtered));   //stringify is converting sting to object
+    })
+})
+
+module.exports = router
