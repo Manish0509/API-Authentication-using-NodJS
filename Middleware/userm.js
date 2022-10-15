8291383b80ff36003cc3242637bb99b30a3a5ed9
Manish Singh <107620367+Manish0509@users.noreply.github.com>
2022-10-16 00:01:28 +0530

    Create userm.js

---
+++ Middleware/userm.js
@@ -0,0 +1,18 @@
+const { decode } = require('jsonwebtoken')
+const utility = require('../utility/utility')
+
+const userVerification = function (req,res,next) {
+    try {
+        var token = req.headers['authorization']
+        var decoded=utility.JWTDecodeToken(token)
+        console.log(decode)
+        req.userId=decoded.id
+        next()
+      }
+      catch (e) {
+        console.log(e)
+        next(e)
+      }
+}
+
+module.exports = userVerification
